2e585ea467e66086af0fd21719a0bdd74879640c
François BILLIOUD <f.billioud@gmail.com>
2020-03-26 16:13:54 +0100

    First commit
    Signed-off-by: François BILLIOUD <f.billioud@gmail.com>

---
+++ README.md
@@ -0,0 +1,89 @@
+# React Universal Navigation
+
+This package provide a component managing the navigation within your components. It is compatible with both `react-native` and `react-native-web`, making it a good tool for universal apps.
+
+## Why?
+
+The usual approach about navigation for React Native is to copy the hystory system form browsers. The issue is that React concept is not really compatible with the concept of retrieving data from the url. React components should depend only on the props they receive.
+
+I created this component to provide a way of navigating between your screens on mobile and browser, in a way more compatible with React way of things.
+
+## Install
+
+Just run : `npm i react-unavigation`
+
+The lib expects you to have the following peer dependencies in your project:
+
+```
+    "react-native": "^0.61.5",
+    "react": "^16.13.1",
+    "styled-components": "^5.0.1"
+```
+
+I intend to remove the dependency to styled-component in the future. Feel free to propose a PR if you don't want styled-components in your project. That should be pretty easy theoretically.
+
+## Usage
+
+This is how you can use the navigation component:
+
+```javascript
+import Navigation from 'react-unavigation'
+
+const App = () => {
+  const [active, setActive] = useState('home')
+  const toHome = () => setActive('home')
+  const toPage1 = () => setActive('page1')
+
+  return (<Navigation active={active}>
+    <View name='home'>
+      <Text>This is the Home page</Text>
+      <Button title='first page' onPress={toPage1}>
+    </View>
+    <View name='page1'>
+      <Text>This is the first page</Text>
+      <Button title='first page' onPress={toHome}>
+    </View>
+    <AnotherComponent name='page2' toHome={toHome} />
+  </Navigation>)
+}
+```
+
+The steps are simple:
+
+1. Import the Navigation component: `import Navigation from 'react-unavigation'`
+2. Create a state to define which screeen is actually active: `const [active, setActive] = useState('home')`
+3. Provide the Navigation component with the name of the active screen and the list of the children `<Navigation active={active}>...(children)...</Navigation>`
+4. Don't forget to give a name to each child component: `<Child name='pageName' />`
+
+## Options
+
+### __Animation duration__
+
+You can provide an optional `duration` property in *ms* to the `Navigation` component. This will change the duration of the transition between the screens.
+
+## miscellaneous
+
+### __Forwarding actions to children__
+
+I encourage users to never transfer setActive to the children of Navigation but prefer using functions like `toHome` or `toXXXScreen`. This way, if you decide to change a component's name, you won't have to change the string everywhere in your application.
+
+### __Component not found and errors__
+
+Of course, if you forget to add a name to your component or if you call `setActive` with a string that doesn't refer to an existing component, you will get a warning.
+
+### __Nested Navigations__
+
+You can use nested Navigation components to match the logic of your application. By the way, Navigation component can be used to navigate within a portion of a view. You don't need to use it only to control the whole application screen. Look at this:
+
+```
+  <Navbar>
+    <Button onPress={() => setActive("tab1")} title={"To tab 1"}>
+    <Button onPress={() => setActive("tab2")} title={"To tab 2"}>
+    <Button onPress={() => setActive("tab3")} title={"To tab 3"}>
+  </Navbar>
+  <Navigation active={active}>
+    <Tab1 name={"tab1"} />
+    <Tab2 name={"tab2"} />
+    <Tab3 name={"tab3"} />
+  </Navigation>
+```
---
+++ index.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react'
+import * as RN from 'react-native'
+import styled from 'styled-components/native'
+
+import PropTypes from 'prop-types'
+
+const Slider = styled.View`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+  flex-direction: row;
+`
+type WrapProps = {
+  active: boolean;
+  activating: boolean;
+  deactivating: boolean;
+}
+
+const Wrap = styled.View<WrapProps>`
+  display: ${props => (props.active || props.activating) ? 'flex' : 'none'};
+  width: 100%;
+  color: red;
+`
+
+export type Props = {
+  active: string;
+  children: React.ReactElement[];
+  duration: number;
+}
+
+const Navigation = React.forwardRef<RN.View, Props>(({ active, children, duration = 500 }: Props, ref = { current: null }) => {
+  if (!children) return children
+  if (!Array.isArray(children)) return children
+  // We keep only the children of type ReactElement as other children will not be accessible anyway
+  const childrenArray = (children as React.ReactNodeArray).filter(child => child && (child as React.ReactElement).props) as Array<React.ReactElement>
+
+  const activeChildIndex = childrenArray.findIndex(child => child.props.name === active) || 0
+
+  const [activeIndex, setActive] = React.useState(activeChildIndex)
+  const [progress, setProgress] = React.useState('0%')
+  const [offset] = React.useState(new RN.Animated.Value(0))
+
+  const target = React.useRef(activeChildIndex)
+
+  if (childrenArray.find(child => !child.props.name)) console.error('All children must have a name within the Navigation component')
+
+  if (!childrenArray.find(child => child.props.name === active)) console.error(`The Navigation component could not match any child with the name ${active}`)
+
+  /** Show a transition from the activeIndex to the active child. **/
+  function animateTo (index: number) {
+    if (progress || index === target.current) return // If an animation is already in progress, we wait until it is over
+    target.current = index // Register the target
+    // Decide to which direction interpolate to
+    setProgress(offset.interpolate({
+      inputRange: [0, 100],
+      outputRange: (target.current > activeIndex) ? ['0%', '-100%'] : ['-100%', '0%']
+    }) as unknown as string)
+    // Starts the animation
+    RN.Animated.timing(offset, { toValue: 100, duration }).start(() => {
+      setProgress('0%')
+      setActive(index) // Once the animation is over, we mark the new active child
+      offset.setValue(0) // Reset the offset
+      // If another animation occured since the last call we execute the animation to the next child
+      if (activeChildIndex !== index) animateTo(activeChildIndex)
+    })
+  }
+
+  // Run the transition each time the active child changes
+  React.useEffect(() => animateTo(activeChildIndex), [active])
+  React.useEffect(() => () => offset.stopAnimation(), [])// Clean up
+
+  return (
+    <Slider ref={ref}>
+      <RN.Animated.View style={{
+        position: 'relative',
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        width: '100%',
+        left: progress
+      }}>
+        {childrenArray.map((child, i) => (
+          <Wrap
+            key={i}
+            active={activeIndex === i}
+            activating={target.current === i}
+            deactivating={activeIndex !== target.current}
+          >
+            {child}
+          </Wrap>
+        ))}
+      </RN.Animated.View>
+    </Slider>
+  )
+})
+
+Navigation.propTypes = {
+  active: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.element.isRequired).isRequired,
+  duration: PropTypes.number.isRequired
+}
+
+Navigation.displayName = 'Navigation'
+
+export default Navigation
